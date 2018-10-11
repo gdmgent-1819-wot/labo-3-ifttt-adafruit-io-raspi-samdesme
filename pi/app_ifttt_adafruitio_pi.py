b34206575f2dcb9554a33a766cdbff7fe1b97d54
“samdesme” <samdesme@student.arteveldehs.be>
2018-10-11 22:28:15 +0200

    final push

--- pi/app_ifttt_adafruitio_pi.py
+++ pi/app_ifttt_adafruitio_pi.py
@@ -2,23 +2,53 @@
 import random
 import sys
 import time
+import pygame
+import threading
+import timeit
+import datetime
 
 # Import Adafruit IO MQTT client.
 from Adafruit_IO import MQTTClient
-from Adafruit_IO import Client
+from Adafruit_IO import Client, Data
+
+from threading import Thread
+from sense_emu import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+from random import randint
+from time import sleep
+from signal import pause
 
 # Set to your Adafruit IO key & username.
 ADAFRUIT_IO_KEY = 'cf3df94c4c2d42159f32b14da19554d2'
 ADAFRUIT_IO_USERNAME = 'samdesme'
-aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 # Feeds 
 group_feed_btn = 'btnpressed'
-data = aio.data(group_feed_btn)
+group_feed_time = 'Reactie-tijd'
 
+sense = SenseHat()
+start = time.time()
+
+def sparkles() :
+    while True:
+        x = randint(0, 7)
+        y = randint(0, 7)
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        sense.set_pixel(x, y, r, g, b)
+        sleep(0.01)
+        
+def playSound() :
+    while True:
+        pygame.mixer.init()
+        pygame.mixer.music.load("analog.wav")
+        pygame.mixer.music.play()
+    
 def connected(client):
+    
     # Subscribe to feed where you want to detect changes from
-    client.subscribe(group_feed_btn) 
+    client.subscribe(group_feed_btn)
+    client.subscribe(group_feed_time) 
     print('Listening for changes on ', group_feed_btn)
 
 
@@ -28,8 +58,36 @@ def disconnected(client):
 
 def message(client, feed_id, payload):
     if payload == "button_pressed":
-        print ("Button was pressed!")
         
+        # define globale variable
+        global start
+        
+        # get timestamp when button is pressed
+        print ("Button was pressed!")
+        start = time.time()
+        
+        # start matrix & play sound
+        Thread(target = sparkles).start()
+        Thread(target = playSound).start()
+        
+        # return value of variable
+        return start
+
+def joystickMiddle(event):
+    if event.action == ACTION_RELEASED:
+        
+        # new timestamp
+        end = time.time()
+        
+        # calculate reaction time
+        timeVal = end - start
+        
+        # publish value to feed
+        client.publish(group_feed_time, timeVal)
+        client.disconnect()
+
+sense.stick.direction_middle = joystickMiddle
+
 # Create an MQTT client instance.
 client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
